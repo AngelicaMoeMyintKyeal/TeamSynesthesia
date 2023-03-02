d92a677dd42eb8c8eeded303d4bec9058dbaabe7
Htet Myat Moe Myint Kyeal <htetmyak.m.m.kyal@gmail.com>
2023-03-02 11:04:28 +0100

    readded files

---
+++ Amotus Vessel/Amotus Vessel/Extension/LocalizationSystem.swift
@@ -0,0 +1,18 @@
+//
+//  LocalizationSystem.swift
+//  Gamemotion
+//
+//  Created by Htet Myat Moe Myint Kyeal on 27/02/23.
+//
+
+import Foundation
+import SwiftUI
+
+class LocalizationSystem: NSObject {
+    
+    static let Localizer = LocalizationSystem()
+    
+    func localize() {
+        
+    }
+}
---
+++ Amotus Vessel/Amotus Vessel/Utilities/ParameterConstants.swift
@@ -0,0 +1,19 @@
+//
+//  ParameterConstants.swift
+//  Gamemotion
+//
+//  Created by Htet Myat Moe Myint Kyeal on 28/02/23.
+//
+
+import Foundation
+
+struct ParameterConstants {
+    
+    static let englishLanguage: String = "en"
+    static let italianLanguage: String = "it"
+    static let koreanLanguage: String = "ko"
+    
+    static let englishPrologueVid: String = "PrologueEng"
+    static let italianPrologueVid: String = "PrologueIta"
+    static let koreanPrologueVid: String = "PrologueKorea"
+}
