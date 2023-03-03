f3621ab6e6be554c111993212094ad843d4e824e
Htet Myat Moe Myint Kyeal <htetmyak.m.m.kyal@gmail.com>
2023-03-03 09:53:03 +0100

    Added spark particles in menu view

--- Amotus Vessel/Amotus Vessel/View/MenuView.swift
+++ Amotus Vessel/Amotus Vessel/View/MenuView.swift
@@ -6,41 +6,60 @@
 //
 
 import SwiftUI
+import SpriteKit
 
 struct MenuView: View {
+    
+    var scene: SKScene {
+            let scene = OnboardingParticleScene()
+            scene.scaleMode = .resizeFill
+            scene.backgroundColor = .clear
+            return scene
+        }
+    
     var body: some View {
     
-        GeometryReader { geometry in
-            VStack {
-                Spacer()
-                Text("Amotus Vessel")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                Spacer()
-                Button(
-                    action: {
-                        // write action
-                        print("Play Button is Clicked")
-                    },
-                    label: {
-                            Text("Play Game")
-                            .frame(
-                                maxWidth: geometry.size.width / 3)
-                    }
-                )
-//                .buttonStyle(.borderedProminent)
-                .frame(height: 44)
-                .padding(.horizontal, 20)
-                .cornerRadius(8)
-                Spacer()
-            }
-            .frame(width: geometry.size.width, height: geometry.size.height)
+        ZStack {
+            
+            GeometryReader { geometry in
+                
+                SpriteView(scene: scene, options: [.allowsTransparency])
+                    .ignoresSafeArea(.all)
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                
+                VStack {
+                    
+                    Spacer()
+                    Text("Amotus Vessel")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    Spacer()
+                    
+                    Button(
+                        action: {
+                            // write action
+                            print("Play Button is Clicked")
+                        },
+                        label: {
+                                Text("Play Game")
+                                .frame(
+                                    maxWidth: geometry.size.width / 3)
+                        }
+                    )
+    //                .buttonStyle(.borderedProminent)
+                    .frame(height: 44)
+                    .padding(.horizontal, 20)
+                    .cornerRadius(8)
+                    Spacer()
                 }
-        .background(
-            Image("MenuBackground")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
+                .frame(width: geometry.size.width, height: geometry.size.height)
+                    }
+            .background(
+                Image("MenuBackground")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
         )
+        }
     }
 }
 
